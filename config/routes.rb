ecd044e679e512177f59861b686f3b0316171d03
Caleb Francis <calebonesimus@gmail.com>
2015-08-06 15:23:50 -0400

    Fix routes and set up root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
 
   resources :posts
+
   devise_for :users
 
-  # Will change to sign in
-  root 'home#index'
-  # root 'devise/sessions#new'
+  authenticated :user do
+    devise_scope :user do
+      root to: "devise/posts#show", as: :user_timeline
+    end
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "devise/sessions#new"
+    end
+  end
 
 end
