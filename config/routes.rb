9a409d1e125957bdf0f74c6c4a7ff9f1f403755c
tsherlock86 <tadsherlock@gmail.com>
2015-08-06 14:48:21 -0400

    added start to wicked

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
 
   # Will change to sign in
   root 'home#index'
   # root 'devise/sessions#new'
+
+  #route for sign-up wizard
+  resources :after_signup
 
 end
