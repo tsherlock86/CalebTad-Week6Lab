c8706c3e43177ed6f82e0ce9734277ebd83b2bef
Caleb Francis <calebonesimus@gmail.com>
2015-08-07 09:39:31 -0400

    Fix some styling on static home

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+testuser = User.create(email: "test@user.com",
+            username: "testuser",
+            first_name: "test",
+            last_name: "user",
+            password: "password",
+            password_confirmation: "password",
+            bio: "I am a test user.")
+
+testuser.posts << Post.create(body: "This post belongs to test user.")            
