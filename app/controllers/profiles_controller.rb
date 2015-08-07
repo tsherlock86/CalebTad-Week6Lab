803ecba7aec2d5bb41937724b77f5e2ee1da07fe
tsherlock86 <tadsherlock@gmail.com>
2015-08-07 14:05:50 -0400

    Added partials to Profile view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,12 +11,15 @@ class ProfilesController < ApplicationController
     if current_user
       if current_user == @user
         flash[:error] = "You cannot follow yourself."
+        redirect_to :back
       else
         current_user.follow(@user)
         flash[:notice] = "You are now following #{@user.username}."
+        redirect_to :back
       end
     else
       flash[:error] = "You must Sign In to follow #{@user.username}."
+      redirect_to :back
     end
   end
 
