ff2177f59cc4049bba40985a51bf36d28261cf5e
Caleb Francis <calebonesimus@gmail.com>
2015-08-09 15:14:44 -0400

    Hastags with a '~' now work

--- app/models/squawk.rb
+++ app/models/squawk.rb
@@ -1,11 +1,27 @@
 class Squawk < ActiveRecord::Base
   belongs_to :user
   validates_length_of :body, maximum: 140
+  before_save :replace_with_link
 
   def self.timeline(user)
     following_ids = user.all_following.collect(&:id)
     all_ids = following_ids << user.id
     Squawk.where(user_id: all_ids).order("created_at DESC")
   end
-  
+
+  def replace_with_link
+    if /~([A-Za-z0-9]+)/.match(self.body)
+      self.body.gsub!(/~([A-Za-z0-9]+)/, create_search_link(get_match))
+    end
+  end
+
+  def create_search_link(string)
+    %Q{<a href="/search/#{string}">#{string}</a>}
+  end
+
+  def get_match
+    /~([A-Za-z0-9]+)/.match(self.body)
+  end
+
+
 end
